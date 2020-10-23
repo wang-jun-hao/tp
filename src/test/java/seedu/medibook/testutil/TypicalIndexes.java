851e6344047e41d7e81f1d7adc7790d1fb546fa5
wang-jun-hao <wang_jun_hao@yahoo.com>
2020-10-24 05:09:11 +0800

    Add public constant fields in test utility classes for testing purposes

--- src/test/java/seedu/medibook/testutil/TypicalIndexes.java
+++ src/test/java/seedu/medibook/testutil/TypicalIndexes.java
@@ -9,4 +9,6 @@ public class TypicalIndexes {
     public static final Index INDEX_FIRST_PATIENT = Index.fromOneBased(1);
     public static final Index INDEX_SECOND_PATIENT = Index.fromOneBased(2);
     public static final Index INDEX_THIRD_PATIENT = Index.fromOneBased(3);
+    public static final Index INDEX_FIRST_MEDICAL_NOTE = Index.fromOneBased(1);
+    public static final Index INDEX_SECOND_MEDICAL_NOTE = Index.fromOneBased(2);
 }
