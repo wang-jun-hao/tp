494d9e8b725caadeafd894bc9f3326677a6f04b7
FooJingYi <foojingyi@comp.nus.edu.sg>
2020-10-27 16:39:27 +0800

    Complete Condition and Treatment classes

--- src/main/java/seedu/medibook/model/medicaldetail/Condition.java
+++ src/main/java/seedu/medibook/model/medicaldetail/Condition.java
@@ -1,2 +1,20 @@
-package seedu.medibook.model.medicaldetail;public class Condition {
+package seedu.medibook.model.medicaldetail;
+
+public class Condition extends Tag {
+
+    /**
+     * Constructs a medical {@code Condition}.
+     *
+     * @param tagName A valid tag name.
+     */
+    public Condition(String tagName) {
+        super(tagName);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+            || (other instanceof Condition // instanceof handles nulls
+            && tagName.equals(((Condition) other).tagName)); // state check
+    }
 }
--- src/main/java/seedu/medibook/model/medicaldetail/Treatment.java
+++ src/main/java/seedu/medibook/model/medicaldetail/Treatment.java
@@ -1,2 +1,20 @@
-package seedu.medibook.model.medicaldetail;public class Treatment {
+package seedu.medibook.model.medicaldetail;
+
+public class Treatment extends Tag {
+
+    /**
+     * Constructs a medical {@code Treatment}.
+     *
+     * @param tagName A valid tag name.
+     */
+    public Treatment(String tagName) {
+        super(tagName);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+            || (other instanceof Treatment // instanceof handles nulls
+            && tagName.equals(((Treatment) other).tagName)); // state check
+    }
 }
