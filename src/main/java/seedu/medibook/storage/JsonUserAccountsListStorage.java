263749d8597edbacf75ad267b7f1e487eae5bb4d
Preston Toh <prestontoh@gmail.com>
2020-10-31 23:18:48 +0800

    Fix bugs

--- src/main/java/seedu/medibook/storage/JsonUserAccountsListStorage.java
+++ src/main/java/seedu/medibook/storage/JsonUserAccountsListStorage.java
@@ -59,13 +59,6 @@ public class JsonUserAccountsListStorage implements UserAccountsListStorage {
 
         requireNonNull(doctorName);
         requireNonNull(doctorMcr);
-        if (!Name.isValidName(doctorName)) {
-            throw new IllegalValueException(Name.MESSAGE_CONSTRAINTS);
-        }
-
-        if (!Mcr.isValidMcr(doctorMcr)) {
-            throw new IllegalValueException(Mcr.MESSAGE_CONSTRAINTS);
-        }
 
         if (username.length() < 5) {
             throw new IllegalValueException(Account.MESSAGE_USERNAME_CONSTRAINTS);
@@ -75,14 +68,26 @@ public class JsonUserAccountsListStorage implements UserAccountsListStorage {
             throw new IllegalValueException(Account.MESSAGE_PASSWORD_CONSTRAINTS);
         }
 
+        if (!Name.isValidName(doctorName)) {
+            throw new IllegalValueException(Name.MESSAGE_CONSTRAINTS);
+        }
+
+        if (!Mcr.isValidMcr(doctorMcr)) {
+            throw new IllegalValueException(Mcr.MESSAGE_CONSTRAINTS);
+        }
+
         Account newAccount = new Account(username, password, new Doctor(new Name(doctorName), new Mcr(doctorMcr)));
+
+        if (newAccount.getUsername().equals("admin")) {
+            throw new IllegalValueException("Username already exists");
+        }
 
         if (FileUtil.isFileExists(filepath)) {
             JsonSerializableUserAccountsList jsonUserAccountList = JsonUtil.readJsonFile(
                     filepath, JsonSerializableUserAccountsList.class).get();
             UserAccountsList accountsList = jsonUserAccountList.toModelType();
 
-            if (accountsList.usernameExists(newAccount) || newAccount.getUsername().equals("admin")) {
+            if (accountsList.usernameExists(newAccount)) {
                 throw new IllegalValueException("Username already exists");
             }
 
